5e61fc1afec4d160156b415bd3b89a8e46d09d87
Eduardo Lacerda <dudu.lacerda.dl@gmail.com>
2020-10-13 01:31:12 -0300

    Início do curso

---
+++ testes/Teste01.py
@@ -0,0 +1,5 @@
+name = input('Digite seu nome: ')
+years = input('Digite sua idade: ')
+weigth = input('Digite seu peso: ')
+
+print('Meu nome é', name, 'e tenho', years, 'anos, e peso', weigth, 'Kg')
---
+++ testes/Teste04.py
@@ -0,0 +1,4 @@
+num1 = int(input('Número 1: '))
+num2 = int(input('Número 2: '))
+
+print('A soma entre o número', num1, 'e', num2, 'é', num1+num2)
